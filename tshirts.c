f9b226c549cc993f34ca48c895715ccd1f0d5851
Venugopal K <venu.shastri@gmail.com>
2024-07-25 06:27:44 +0530

    Create tshirts.c

---
+++ tshirts.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <assert.h>
+
+char size(int cms) {
+    char sizeName = '\0';
+    if(cms < 38) {
+        sizeName = 'S';
+    } else if(cms > 38 && cms < 42) {
+        sizeName = 'M';
+    } else if(cms > 42) {
+        sizeName = 'L';
+    }
+    return sizeName;
+}
+
+int main() {
+    assert(size(37) == 'S');
+    assert(size(40) == 'M');
+    assert(size(43) == 'L');
+    printf("All is well (maybe!)\n");
+    return 0;
+}
